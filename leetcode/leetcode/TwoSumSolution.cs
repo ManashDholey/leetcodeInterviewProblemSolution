4973e2821adba90a27f9fd30a6b4b6de91b14ad2
Manash <Manash@DESKTOP-H7BTIBJ>
2023-09-16 21:46:31 +0530

    Add solution for sorted array.

--- leetcode/leetcode/TwoSumSolution.cs
+++ leetcode/leetcode/TwoSumSolution.cs
@@ -15,10 +15,35 @@ namespace leetcode
 
         public int[] TwoSum(int[] nums, int target)
         {
-            return TwoSumFun(nums, target);
+           // return TwoSumFun(nums, target);
+            return TwoSumFunBest(nums, target);
         }
+
+        private int[] TwoSumFunBest(int[] nums, int target)
+        {  //Time Complexcity :O(nlogn) Space:C(1)
+            // this solution working for sorted array
+            Array.Sort(nums);
+            int sum = 0;    
+            int left=0, right=nums.Length-1;
+            while (left < right) {
+                sum = nums[left] + nums[right];
+                if (sum == target) {
+                    return new int[]{ left,right};
+                }
+                else if (sum < target) {
+                    left += 1;
+                }
+                else
+                {
+                    right -= 1;
+                }
+            }
+            return new int[] { -1, -1 };    
+        }
+
         private int[] TwoSumFun(int[] nums, int target)
         {
+            //Time Complexcity :O(n) Space:C(n)
             // Create a dictionary to store elements and their indices
             Dictionary<int, int> numIndices = new Dictionary<int, int>();
 

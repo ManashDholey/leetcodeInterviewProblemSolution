026f08396e2a6f883fec737b38bdf520a88519bd
ManashDholey <chottu.dholey52@gmail.com>
2025-08-31 12:48:46 +0530

    Add LongestPalindromicSubstring class with IDisposable
    Introduces the `LongestPalindromicSubstring` class in the `leetcode` namespace, featuring a method to find the longest palindromic substring using center expansion. Implements the `IDisposable` interface for resource management. Updates `Program.cs` to demonstrate the functionality of the new class.

---
+++ leetcode/leetcode/LongestPalindromicSubstring.cs
@@ -0,0 +1,78 @@
+﻿
+
+namespace leetcode
+{
+    public class LongestPalindromicSubstring : IDisposable
+    {
+        private bool disposedValue;
+
+        public  string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length < 1)
+                return "";
+   
+            int start = 0, end = 0;
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                // Odd length palindrome
+                int len1 = ExpandFromCenter(s, i, i);
+                // Even length palindrome
+                int len2 = ExpandFromCenter(s, i, i + 1);
+
+                int len = Math.Max(len1, len2);
+
+                if (len > end - start)
+                {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+
+            return s.Substring(start, end - start + 1);
+        }
+
+        private  int ExpandFromCenter(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+            return right - left - 1;
+        }
+
+
+
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~LongestPalindromicSubstring()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}

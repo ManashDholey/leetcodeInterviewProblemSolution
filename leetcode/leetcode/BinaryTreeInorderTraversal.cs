e13a69aeec73c384bab22ad10b39f80599e33a13
ManashDholey <chottu.dholey52@gmail.com>
2025-09-02 01:19:20 +0530

    Add BinaryTreeInorderTraversal and TreeNode classes
    Introduce `BinaryTreeInorderTraversal` for inorder traversal of binary trees.
    Define `TreeNode` class to represent tree nodes. Update `Program.cs`
    to demonstrate usage of the new classes with a sample binary tree.

---
+++ leetcode/leetcode/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode
+{
+    
+    public class BinaryTreeInorderTraversal : DisposableBase
+    {
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            Inorder(root, result);
+            return result;
+        }
+
+        private void Inorder(TreeNode node, List<int> result)
+        {
+            if (node == null) return;
+            Inorder(node.left, result);
+            result.Add(node.val);
+            Inorder(node.right, result);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Dispose managed resources
+
+            }
+
+            // No unmanaged resources, so nothing extra here
+
+            // Call base to set _disposed flag
+            base.Dispose(disposing);
+        }
+    }
+}

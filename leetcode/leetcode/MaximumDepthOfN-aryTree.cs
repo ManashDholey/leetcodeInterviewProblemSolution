e2d9a99415157816fc59fd040532ea949143fb71
ManashDholey <chottu.dholey52@gmail.com>
2025-09-09 00:20:35 +0530

    commit for Maximum Depth of N-ary Tree

---
+++ leetcode/leetcode/MaximumDepthOfN-aryTree.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace leetcode
+{
+    public class MaximumDepthOfN_aryTree
+    {
+        public int MaxDepth(Node root)
+        {
+            if (root == null) return 0;
+
+            int max = 0;
+            foreach (var child in root.children)
+            {
+                max = Math.Max(max, MaxDepth(child));
+            }
+
+            return max + 1; // add current node depth
+        }
+    }
+}
---
+++ leetcode/leetcode/Node.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace leetcode
+{
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node()
+        {
+            children = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            children = new List<Node>();
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}

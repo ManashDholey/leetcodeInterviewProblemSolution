95982a639fe7990164cdea0f4a2e1b96942f600c
ManashDholey <chottu.dholey52@gmail.com>
2025-09-09 21:13:01 +0530

    commit for path Sum

---
+++ leetcode/leetcode/PathSum.cs
@@ -0,0 +1,67 @@
+﻿
+
+namespace leetcode
+{
+    public class PathSum : DisposableBase
+    {
+
+
+
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+
+            Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+            Stack<int> sumStack = new Stack<int>();
+
+            if (root == null) return false;
+            // If it's a leaf node, check if the remaining targetSum equals the node's value
+            if (root?.left == null && root?.right == null)
+            {
+                return targetSum == root?.val;
+            }
+            nodeStack.Push(root);
+            sumStack.Push(root.val);
+            while (nodeStack != null && nodeStack?.Count() != 0)
+            {
+                TreeNode? currentNode = nodeStack?.Pop();
+                int currentSum = sumStack?.Pop() ?? 0;
+                if (currentNode != null && currentNode?.left == null && currentNode?.right == null && currentSum == targetSum)
+                {
+                    return true;
+                }
+
+                if (currentNode?.right != null)
+                {
+                    nodeStack?.Push(currentNode?.right);
+                    sumStack?.Push(currentSum + currentNode?.right?.val ?? 0);
+                }
+                if (currentNode?.left != null)
+                {
+                    nodeStack?.Push(currentNode?.left);
+                    sumStack?.Push(currentSum + currentNode?.left?.val ?? 0);
+                }
+            }
+            return false;
+        }
+
+
+
+
+
+
+
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Dispose managed resources
+            }
+            // No unmanaged resources, so nothing extra here
+            // Call base to set _disposed flag
+            base.Dispose(disposing);
+        }
+    }
+}
--- leetcode/leetcode/Program.cs
+++ leetcode/leetcode/Program.cs
@@ -171,3 +171,18 @@ Node root4 = new Node(1, new List<Node> {
 maximumDepthOfN.MaxDepth(root4);
 maximumDepthOfN.Dispose();
 
+TreeNode root5 = new TreeNode(5,
+    new TreeNode(4,
+        new TreeNode(11,
+            new TreeNode(7),
+            new TreeNode(2)
+        ),
+        null
+    ),
+    new TreeNode(8,
+        new TreeNode(13),
+        new TreeNode(4, null, new TreeNode(1))
+    )
+);
+PathSum pathSum=new PathSum();
+Console.WriteLine(pathSum.HasPathSum(root5, 22));

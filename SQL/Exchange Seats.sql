1d7f0c075d572eae2bbbe07822e98a873bf5afde
ManashDholey <chottu.dholey52@gmail.com>
2025-09-30 00:07:03 +0530

    commit for Product Sales Analysis I

--- SQL/Exchange Seats.sql
+++ SQL/Exchange Seats.sql
@@ -399,3 +399,39 @@ GO
 SELECT actor_id, director_id FROM ActorDirector
 GROUP BY actor_id, director_id
 HAVING COUNT(actor_id) >=3
+GO
+--Product Sales Analysis I
+CREATE TABLE Product (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50)
+);
+
+CREATE TABLE Sales (
+    sale_id INT,
+    product_id INT,
+    year INT,
+    quantity INT,
+    price INT,
+    PRIMARY KEY (sale_id, year),
+    FOREIGN KEY (product_id) REFERENCES Product(product_id)
+);
+
+INSERT INTO Product (product_id, product_name) VALUES
+(100, 'Nokia'),
+(200, 'Apple'),
+(300, 'Samsung');
+
+INSERT INTO Sales (sale_id, product_id, year, quantity, price) VALUES
+(1, 100, 2008, 10, 5000),
+(2, 100, 2009, 12, 5000),
+(7, 200, 2011, 15, 9000);
+GO
+SELECT 
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales s
+INNER JOIN Product p
+ON s.product_id = p.product_id;
+
+

1aead79084629baea5a1a93017f0d68f66660a5b
ManashDholey <chottu.dholey52@gmail.com>
2025-09-29 23:47:26 +0530

    Actors and Directors Who Cooperated At Least Three Times

--- SQL/Exchange Seats.sql
+++ SQL/Exchange Seats.sql
@@ -378,3 +378,24 @@ SELECT
         ELSE 'No'
     END AS triangle
 FROM Triangle;
+--Actors and Directors Who Cooperated At Least Three Times
+GO
+CREATE TABLE ActorDirector (
+    actor_id INT,
+    director_id INT,
+    timestamp INT PRIMARY KEY
+);
+GO
+INSERT INTO ActorDirector (actor_id, director_id, timestamp) VALUES
+(1, 1, 0),
+(1, 1, 1),
+(1, 1, 2),
+(1, 2, 3),
+(1, 2, 4),
+(2, 1, 5),
+(2, 1, 6);
+GO
+
+SELECT actor_id, director_id FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(actor_id) >=3
